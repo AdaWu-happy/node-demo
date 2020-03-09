a65e37fb2eef2d92776cf244cb21739e9d4e9a75
wuyuting <1289800193@qq.com>
2020-03-09 16:23:36 +0800

    删除js模块化文件.

--- common-js/modules/module2.js
+++
@@ -1,4 +0,0 @@
-// moudle.exports = function(){} 暴露一个函数
-module.exports = function(){
-    console.log('bbb')
-}
--- common-js/modules/module3.js
+++
@@ -1,8 +0,0 @@
-// exports.xxx = value
-exports.foo = function(){
-    console.log('foo() ccc')
-}
-exports.bar = function(){
-    console.log('bar() ddd')
-}
-exports.arr = [1,2,3,3,4,6,8,10,12,12]
